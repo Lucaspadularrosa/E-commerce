b5907d4b6ce1daeedb364f71579859fb220df52d
Lucaspadularrosa <lucas_sfc98@outlook.com>
2021-08-21 12:48:30 -0300

    Actualizacion nro 4

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ const btn = document.querySelectorAll('.heart');
 const offCanvas = document.querySelector('.offcanvas-body');
 const span = document.querySelector('.span');
 const badge = document.querySelector('.badge');
+const pay = document.querySelector('.pay');
+const modalBody = document.querySelector('.modal-body');
+const modalPrice = document.querySelector('.modal-price');
+console.dir(modalPrice);
 badge.innerHTML = 0;
 
-console.log(span);
-console.dir(window);
-console.dir(window.localStorage.username);
 const user = window.localStorage.username;
 if (user === undefined) {
   span.innerHTML = '';
@@ -22,8 +23,6 @@ for (let i = 0; i < btn.length; i++) {
   chil.push(btn[i].children[0]);
 }
 
-console.dir(chil[0]);
-console.dir(chil);
 let total = 0;
 
 chil.map((number) =>
@@ -54,14 +53,52 @@ chil.map((number) =>
       </div>`;
       total += priceParse;
       badge.innerHTML = '$' + total;
+      modalPrice.innerHTML = '$' + total;
+      modalBody.innerHTML += `<div class="card mb-3" style="max-width: 940px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${img}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p> $${price}</p>
+            <div class="btn-toolbar mt-3" role="toolbar" aria-label="Toolbar with button groups">
+            <div class="btn-group me-2" role="group" aria-label="First group">
+            <button type="button" class="btn btn-primary">37</button>
+            <button type="button" class="btn btn-primary">38</button>
+            <button type="button" class="btn btn-primary">39</button>
+            <button type="button" class="btn btn-primary">40</button>
+            <button type="button" class="btn btn-primary">41</button>
+            <button type="button" class="btn btn-primary">42</button>
+            </div>
+            <div class="btn-group">
+            <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+           Cantidad
+          </button>
+          <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">1</a></li>
+          <li><a class="dropdown-item" href="#">2</a></li>
+          <li><a class="dropdown-item" href="#">3</a></li>
+          <li><a class="dropdown-item" href="#">4</a></li>
+          <li><a class="dropdown-item" href="#">5</a></li>
+          </div>
+            <p class="card-text mt-3">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+    </div>`;
     } else {
       for (let i = 0; i < offCanvas.childNodes.length; i++) {
         if (numberId === offCanvas.childNodes[i].id) {
           offCanvas.childNodes[i].innerHTML = null;
+          modalBody.childNodes[i].innerHTML = '';
         }
       }
       total -= priceParse;
       badge.innerHTML = '$' + total;
+      modalPrice.innerHTML = '$' + total;
     }
   })
 );
@@ -69,7 +106,6 @@ chil.map((number) =>
 const buscador = (e) => {
   e.preventDefault();
   let value = search.value.toLowerCase().trim();
-  console.log(value);
   for (let i = 0; i < myH2.length; i++) {
     if (myH2[i].textContent.toLocaleLowerCase().indexOf(value) === -1) {
       myH2[i].classList.add('visually-hidden');
---
+++ productos_carrito/index.js
@@ -0,0 +1,51 @@
+const form = document.querySelector('.form');
+const formControl = document.querySelectorAll('.form-control');
+const formCheck = document.querySelector('.form-check-input');
+const btnCompras = document.querySelector('.btn-compras');
+const camposObligatorios = document.querySelector('.camposObligatorios');
+const terminos = document.querySelector('.terminos');
+
+const arrLabel = [];
+const objLabel = {
+  checked: false,
+};
+
+formControl.forEach((label) => {
+  arrLabel.push(label);
+});
+
+btnCompras.addEventListener('click', (e) => {
+  e.preventDefault();
+  for (let i = 0; i < arrLabel.length; i++) {
+    if (arrLabel[i].value === '') {
+      camposObligatorios.innerHTML = 'Campos Obligatorios *';
+    } else {
+      camposObligatorios.innerHTML = '';
+      arrLabel[i].checked = true;
+    }
+  }
+
+  if (formCheck.checked === false) {
+    terminos.innerHTML = 'Aceptar términos y condiciones';
+  } else {
+    terminos.innerHTML = '';
+    objLabel.checked = true;
+  }
+
+  for (let i = 0; i < arrLabel.length; i++) {
+    if (arrLabel[i].checked === false) {
+      console.log('error');
+      return;
+    }
+  }
+
+  if (objLabel.checked === false) {
+    console.log('error2');
+    return;
+  }
+  // form.addEventListener('submit', () => {
+  // });
+  console.dir(objLabel.checked);
+  console.log('todo piola');
+  window.location = '../index.html';
+});
